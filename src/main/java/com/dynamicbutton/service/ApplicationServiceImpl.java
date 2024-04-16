91117d4503780d21983e45f276dae4c4b26cdcc7
Gaurav <graj0733@gmail.com>
2024-04-16 22:06:21 +0530

    ApplicationMenu

--- src/main/java/com/dynamicbutton/service/ApplicationServiceImpl.java
+++ src/main/java/com/dynamicbutton/service/ApplicationServiceImpl.java
@@ -4,12 +4,14 @@ import com.dynamicbutton.entity.ApplicationEntity;
 import com.dynamicbutton.entity.ApplicationMenuEntity;
 import com.dynamicbutton.repo.ApplicationMenuRepo;
 import com.dynamicbutton.repo.ApplicationRepo;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -35,7 +37,39 @@ public class ApplicationServiceImpl implements ApplicationService {
         Map<String,Object> result = new HashMap<>();
         Long applicationId = Long.parseLong( String.valueOf(map.get("applicationid")));
         List<ApplicationMenuEntity> applicationEntity =applicationMenuRepo.findAllByApplicationid(applicationId);
-        result.put("applicationmenu",applicationEntity);
+        Map<String,ApplicationMenuEntity> applicationMenuMap = new HashMap<>();
+        Map<String,List<Map<String,Object>>> applicationMenuMapp = new HashMap<>();
+        if(applicationEntity != null && applicationEntity.size()>0){
+            for(ApplicationMenuEntity applicationMenuEntity : applicationEntity){
+                List<Map<String,Object>> applicationmenulist = new LinkedList<>();
+                Map<String,Object> entityMap = new HashMap<>();
+                String applicationPath = applicationMenuEntity.getPath();
+                Long id = applicationMenuEntity.getId();
+                String actionurl = applicationMenuEntity.getActionurl();
+                Long applicationid = applicationMenuEntity.getApplicationid();
+                Long mainmenuid = applicationMenuEntity.getMainmenuid();
+                String label = applicationMenuEntity.getLabel();
+
+                entityMap.put("path",applicationPath);
+                entityMap.put("id",id);
+                entityMap.put("actionurl",actionurl);
+                entityMap.put("applicationid",applicationId);
+                entityMap.put("label",label);
+
+                if(!applicationMenuMapp.containsKey(applicationPath)){
+                    applicationmenulist.add(entityMap);
+                    applicationMenuMapp.put(applicationPath,applicationmenulist);
+                }else{
+                    applicationmenulist =applicationMenuMapp.get(applicationPath);
+                    applicationmenulist.add(entityMap);
+                    applicationMenuMapp.put(applicationPath,applicationmenulist);
+                }
+
+            }
+
+        }
+
+        result.put("applicationmenu",applicationMenuMapp);
         return result;
     }
 }
